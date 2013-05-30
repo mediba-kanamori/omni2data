05f8f57b7574684f8676ecfc9bde224bff5c270e
uknmr <y.kinmori@gmail.com>
2013-05-31 08:32:08 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Omni2data::Application.config.secret_key_base = '9dbd344401e4a361197724f4da80db9b88133e6f383397e5f4e94640dbc54d8f81a211bd54c367e504d8fa050b8ff0e1dd1faa19fcca52802dfd797e4b857522'
